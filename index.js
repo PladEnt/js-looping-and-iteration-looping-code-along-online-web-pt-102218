423cf6b64b4bdbef707bce02b9be81f111b4ed2b
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-06-04 17:47:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(names, evint){
+  for (let i = 0; i < names.length; i++) {
+    console.log(`"Thank you, ${names[i]}, for the wonderful birthday gift.",
+  "Thank you, Brendan, for the wonderful ${evint} gift.`);
+  }
+ 
+  return names;
+}
